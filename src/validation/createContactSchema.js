00026cc850640484cde400361e6915e801c5d6b6
Anastasiia262 <gribskay2@gmail.com>
2024-10-29 15:18:46 +0300

    Fix validation modules import issue and resolve package schema error

--- src/validation/createContactSchema.js
+++ src/validation/createContactSchema.js
@@ -1,10 +1,14 @@
 import Joi from 'joi';
 import { phoneNumberRegex } from '../constants/constants.js';
 
-export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().pattern(phoneNumberRegex),
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().pattern(phoneNumberRegex).required(),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('work', 'home', 'personal')
+    .required(),
 });
